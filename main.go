5bb415ee5be6292ee028606c9ce34ceabfb32963
lukmandenny <lukmandennylazuardian@gmail.com>
2024-04-26 17:04:20 +0700

    Tambah Path Variable dan Query String

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main() {
 
 	r.GET("/", rootHandler)
 	r.GET("/helloworld", helloHandler)
+	r.GET("/books/:id/:title", booksHandler)
+	r.GET("/query", queryHandler)
 
 	r.Run(":8888")
 
@@ -31,4 +33,25 @@ func helloHandler(ctx *gin.Context) {
 		"name":    "Denny",
 	}
 	ctx.JSON(http.StatusOK, data)
+}
+
+func booksHandler(ctx *gin.Context) {
+	id := ctx.Param("id")
+	title := ctx.Param("title")
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"id":    id,
+		"title": title,
+	})
+}
+
+func queryHandler(ctx *gin.Context) {
+	// Menggunakan ctx.Query("title") untuk menangkap nilai dari parameter query "title"
+	title := ctx.Query("title")
+	harga := ctx.Query("harga")
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"title": title,
+		"harga": harga,
+	})
 }
