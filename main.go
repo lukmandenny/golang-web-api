bf3dff00e444190b882d970ac91095368a947ec0
lukmandenny <lukmandennylazuardian@gmail.com>
2024-04-26 15:00:47 +0700

    ubah anonymous function jadi named function

--- main.go
+++ main.go
@@ -9,14 +9,26 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.GET("/helloworld", func(ctx *gin.Context) {
-		// Menggunakan gin.H untuk mengirim response JSON
-		data := gin.H{
-			"message": "Hello World! My Name is",
-			"name":    "Denny",
-		}
-		ctx.JSON(http.StatusOK, data)
-	})
+	r.GET("/", rootHandler)
+	r.GET("/helloworld", helloHandler)
+
 	r.Run(":8888")
 
+}
+
+func rootHandler(ctx *gin.Context) {
+	// Menggunakan gin.H untuk mengirim response JSON
+	data := gin.H{
+		"message": "Route URL",
+	}
+	ctx.JSON(http.StatusOK, data)
+}
+
+func helloHandler(ctx *gin.Context) {
+	// Menggunakan gin.H untuk mengirim response JSON
+	data := gin.H{
+		"message": "Hello World! My Name is",
+		"name":    "Denny",
+	}
+	ctx.JSON(http.StatusOK, data)
 }
